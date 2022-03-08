3127cbb18c445ba329470a8da56ac56a9a9355fc
codesamuraii <remi@heneault.fr>
2022-03-08 04:09:39 +0100

    Command line working

--- tomem.py
+++ tomem.py
@@ -1,5 +1,5 @@
 import argparse
-from filestore import *
+from tomem.memstore import *
 
 
 def get_command_arguments():
@@ -9,22 +9,15 @@ def get_command_arguments():
         formatter_class=argparse.RawTextHelpFormatter,
     )
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--retrieve', metavar='id', help='ID of the file to retrieve')
+    group.add_argument('--retrieve', metavar=('id', 'dest'), nargs='+', help='ID of the file to retrieve')
     group.add_argument('--store', metavar=('file', 'id'), nargs='+', help='file to store with optional custom ID')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = get_command_arguments()
-    store_args = args.store
-    retrieve_args = args.retrieve
 
-    if store_args:
-        file = store_args.pop(0)
-        custom_id = store_args.pop() if store_args else None
-        storage_id = store_in_memory(file, custom_id)
-        print(f'File stored with ID: {storage_id}')
+    if args.store:
+        store_in_memory(*args.store)
     else:
-        storage_id = retrieve_args
-        file_name = retrieve_file(storage_id)
-        print(f'File with ID {storage_id} retrieved: {file_name}')
+        retrieve_file(*args.retrieve)
