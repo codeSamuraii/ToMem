83b55c4cc484c4fad1cac87db7272f0cabd150db
codesamuraii <remi@heneault.fr>
2022-03-10 21:15:06 +0100

    more doc

--- tomem/__init__.py
+++ tomem/__init__.py
@@ -1,6 +1,15 @@
 """Store files in memory with `memcached`_.
 
-A Python API is available through the `MemStore` class.
+Uses a ledger to keep track of stored files informations such as name, size and checksum
+to ensure data integrity. A custom uid can be provided when storing a file, otherwise a human-readable
+one will be provided.
+
+Examples:
+
+    $ python tomem --store images/file10.png
+    * butcheress - file10.png
+
+A Python API is available through the :py:class:`tomem.api.MemStore` class.
 
 .. _memcached: https://memcached.org/
 
