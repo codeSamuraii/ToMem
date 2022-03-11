570c6acbc997ff2f57f0916d6e923aa575b6490e
codesamuraii <remi@heneault.fr>
2022-03-12 00:06:04 +0100

    Better module doc

--- tomem/__init__.py
+++ tomem/__init__.py
@@ -1,13 +1,27 @@
 """Store files in memory with `memcached`_.
 
-Uses a ledger to keep track of stored files informations such as name, size and checksum
-to ensure data integrity. A custom uid can be provided when storing a file, otherwise a human-readable
-one will be provided.
+Uses a ledger to keep track of stored files informations such as name, size and
+checksum to ensure data integrity.
+A custom uid can be provided (using the `uid::/file/path` format) otherwise a
+human-readable one will be provided.
+
+_**This is a personal project and a work in progress.**_
 
 Examples:
 
-    $ python tomem --store images/file10.png
-    * butcheress - file10.png
+    $ python tomem --store images/cat.png ./report.xls customid::/tmp/data.bin
+
+    $ python tomem --retrieve butcheress oversteer
+    $ ls -l
+    cat.png report.xls
+
+    $ python tomem --list
+    * butcheress - cat.png
+    * oversteer - report.xls
+    * customid - data.bin
+
+    $ python tomem --flush-all
+    3 items deleted freeing 1.4MiB
 
 A Python API is available through the :py:class:`tomem.api.MemStore` class.
 
