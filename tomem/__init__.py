e2b424603a74c8d55086905c1da7ff801efd8a91
codesamuraii <remi@heneault.fr>
2022-03-12 03:45:50 +0100

    doc

--- tomem/__init__.py
+++ tomem/__init__.py
@@ -5,25 +5,30 @@ checksum to ensure data integrity.
 A custom uid can be provided (using the `uid::/file/path` format) otherwise a
 human-readable one will be provided.
 
-_**This is a personal project and a work in progress.**_
+*This is a personal project and a work in progress.*
 
 Examples:
+::
 
+    # Store files (specify custom uid with `::` prefix)
     $ python tomem --store images/cat.png ./report.xls customid::/tmp/data.bin
 
+    # Retrieve files using their uids
     $ python tomem --retrieve butcheress oversteer
     $ ls -l
     cat.png report.xls
 
+    # List stored files
     $ python tomem --list
     * butcheress - cat.png
     * oversteer - report.xls
     * customid - data.bin
 
+    # Wipe every file from memory
     $ python tomem --flush-all
     3 items deleted freeing 1.4MiB
 
-A Python API is available through the :py:class:`tomem.api.MemStore` class.
+A Python API is available through the :class:`~tomem.api.memstore.MemStore` class.
 
 .. _memcached: https://memcached.org/
 
